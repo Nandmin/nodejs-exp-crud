bb1016441405d483b5ae14d1c8c2f6681e262f2f
Nandmin <andras78.nemeth@gmail.com>
2022-02-11 23:22:28 +0100

    navbar

---
+++ front/src/app/nav/nav.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+
+  links: {title: string, url: string}[] = [
+    {title: 'Home', url: '/'},
+    {title: 'Users', url: '/users'},
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
